be101df76e07a8953097985778c74c108be2c104
bishnu.email.usa@gmail.com <bishnu.email.usa@gmail.com>
2024-02-12 18:28:15 -0500

    remove console from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import FaqSection from "./FaqSection";
-import backgroundimg from "../src/images/background-pattern-mobile.svg"
+import backgroundimg from "../src/images/background-pattern-mobile.svg";
 
 const App = () => {
-  console.log(backgroundimg, 'img')
- 
   return (
     <div className="flex justify-center mt-[100px]">
-      <div className="w-full h-[250px] absolute top-0 -z-50 bg-no-repeat bg-cover" style={{backgroundImage: `url(${backgroundimg})`}}>
-        
-      </div>
+      <div
+        className="w-full h-[250px] absolute top-0 -z-50 bg-no-repeat bg-cover"
+        style={{ backgroundImage: `url(${backgroundimg})` }}
+      ></div>
       <div className="bg-slate-200 w-[600px] h-auto rounded-md shadow-lg m-5 md:m-1">
         <div className="mt-4 px-4 font-extrabold text-2xl flex gap-3">
           <div className=" text-pink-600">
